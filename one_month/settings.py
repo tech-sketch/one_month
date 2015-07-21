8be1b878801b2deebd1fc2ad676f7af34fe283aa
ko-he- <tie304355@S1504C10257-T1.tisnt.tis.co.jp>
2015-07-21 10:02:00 +0900

    merge pr48

--- one_month/settings.py
+++ one_month/settings.py
@@ -102,7 +102,7 @@ DATABASES = {
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'one_month',
-        'USER': 'admin',
+        'USER': 'postgres',
         'PASSWORD': 'admin',
         'HOST': '127.0.0.1',
         'PORT':5432,
